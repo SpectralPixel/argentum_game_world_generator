35dd6969342c7edaf6b25826eb17bb8297ce1e44
Emil Engelbert <encrypteddvjjrxv@gmail.com>
2024-08-20 17:26:08 +0200

    Link workspace dependencies by path

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// use argentum_game_coordinate_system::{Coordinate, CoordinateType, Region};
+// use argentum_game_voxel::Voxel;
+// use ndarray::{Array3, Ix3};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// pub fn generate(position: Coordinate, size: CoordinateType) -> Array3<Voxel> {
+//     let region = Region::new(position, size);
+//     let size = size as usize;
+//     let mut chunk_data = Array3::from_elem(Ix3(size, size, size), Voxel::default());
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//     for position in region {
+//         match position.y {
+//             _ => {
+//                 let voxel = chunk_data.get_mut(Ix3(position.x, position.y, position.z));
+//                 *voxel = Voxel::new(0);
+//             },
+//         }
+//     }
+
+//     chunk_data
+// }
